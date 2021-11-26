7da0a6a512e2b31ee9f7d09d375fd6a5bf54478f
ElmiraT <93406585+Elmiratash@users.noreply.github.com>
2021-11-26 12:59:41 -0800

    ready to push

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+var main = document.querySelector('.main');
+var nav = document.querySelector('.nav');
+
+
+window.onscroll = function() {
+
+    if (window.pageYOffset > (main.offsetHeight - nav.offsetHeight)) {
+        nav.classList.remove('bottom-nav');
+        nav.classList.add('top-nav');
+    } else {
+        nav.classList.add('bottom-nav');
+        nav.classList.remove('top-nav');
+
+    }
+}
